79eea95c87ebb42fb48b122b45d400410618d510
ruso99 <hondongk@naver.com>
2021-09-10 15:03:54 +0900

    [ADD] 테스트로 만들어본거

---
+++ src/components/Home/index.ts
@@ -0,0 +1,1 @@
+export { FirstCard } from './FirstCard';
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { FirstCard } from 'components/Home';
 
 const Home = () => {
-  return <div>HELLO</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: '#7700ff',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <FirstCard />
+    </div>
+  );
 };
 
 export default Home;
