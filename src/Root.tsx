b05bb0d1582cb051523d1b3560fc03f3356a6cc2
ruso99 <hondongk@naver.com>
2021-09-29 14:08:00 +0900

    [ADD] 페이지 추가

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import Home from 'pages/Home';
+import Dongchurl from 'pages/dongchurl/Dongchurl';
 
 function Root() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/dongchurl" component={Dongchurl} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/pages/dongchurl/Dongchurl.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dongchurl = () => {
+  return <div>HELLO</div>;
+};
+
+export default Dongchurl;
