7cd9010a9fac99d3b6769a98e7d35597029748eb
UNNEW-main <1970005826c@gmail.com>
2021-10-02 17:14:20 +0900

    Add: Page1 UI작업

--- src/pages/yun/Page1.tsx
+++ src/pages/yun/Page1.tsx
@@ -1,8 +1,69 @@
 import React from 'react';
 import { LayoutContainer } from './components/index';
+import { motion } from 'framer-motion';
+import styled from '@emotion/styled';
 
 const Page1 = () => {
-  return <LayoutContainer></LayoutContainer>;
+  return (
+    <LayoutContainer>
+      <BackgroundImg>
+        <Wrapper>
+          <TitleImg></TitleImg>
+          <Start
+            animate={{
+              opacity: [1, 0, 1],
+            }}
+            transition={{
+              duration: 3,
+              ease: 'linear',
+              repeat: Infinity,
+            }}
+          >
+            Touch To Start
+          </Start>
+          <Exit>Exit</Exit>
+        </Wrapper>
+      </BackgroundImg>
+    </LayoutContainer>
+  );
 };
 
 export default Page1;
+
+const BackgroundImg = styled(motion.div)`
+  background: url('/yun/img/background.jpg');
+  width: 100vw;
+  height: 100vh;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+const Wrapper = styled.div`
+  margin-right: 80px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+const TitleImg = styled(motion.div)`
+  background: url('/yun/img/logo.png');
+  width: 320px;
+  height: 250px;
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin-top: 50px;
+`;
+
+const Start = styled(motion.div)`
+  color: white;
+  font-size: 50px;
+  cursor: pointer;
+`;
+
+const Exit = styled(motion.div)`
+  color: white;
+  font-size: 40px;
+  margin-bottom: 50px;
+  cursor: pointer;
+`;
