c0c4a965ed08d160a0a0c5c8bcdb9a90d53a1db4
UNNEW-main <1970005826c@gmail.com>
2021-10-04 19:31:38 +0900

    [ADD]Page2 part2 작업

--- src/pages/yun/Page2.tsx
+++ src/pages/yun/Page2.tsx
@@ -49,21 +49,39 @@ export const Page2: React.FC<Props> = ({ handlePage }) => {
           delay: curState === 0 ? 0 : curState === 2 ? 0 : 0,
         }}
       >
-        <LeftTitle
-          animate={{
-            opacity: curState === 0 ? [0, 1] : 0,
-            y: '20px',
+        <TitleWrapper>
+          <LeftTitle
+            animate={{
+              opacity: curState === 0 ? [0, 1] : 0,
+              y: '20px',
 
-            transitionEnd: curState === 1 ? { display: 'none', width: '0%' } : undefined,
-          }}
-          transition={{
-            duration: 1,
-            ease: 'easeOut',
-            delay: curState === 0 ? 2 : 0,
-          }}
-        >
-          테네시위스키의 자존심
-        </LeftTitle>
+              transitionEnd: curState === 1 ? { display: 'none', width: '0%' } : undefined,
+            }}
+            transition={{
+              duration: 1,
+              ease: 'easeOut',
+              delay: curState === 0 ? 2 : 0,
+            }}
+          >
+            테네시위스키의 자존심
+          </LeftTitle>
+          <RightTitle
+            animate={{
+              opacity: curState === 0 ? [0, 1] : [1, 0],
+              y: '-20px',
+
+              transitionEnd: curState === 1 ? { display: 'none', width: '0%' } : undefined,
+            }}
+            transition={{
+              duration: 1,
+              ease: 'easeOut',
+              delay: curState === 0 ? 2 : 0,
+            }}
+          >
+            잭 다니엘 Old No.7
+          </RightTitle>
+        </TitleWrapper>
+
         <JackImg
           animate={{
             opacity: curState === 0 ? [0, 1] : curState === 2 ? 0 : 1,
@@ -81,50 +99,100 @@ export const Page2: React.FC<Props> = ({ handlePage }) => {
             delay: curState === 0 ? 1 : 0,
           }}
         />
-        <RightTitle
-          animate={{
-            opacity: curState === 0 ? [0, 1] : [1, 0],
-            y: '-20px',
-
-            transitionEnd: curState === 1 ? { display: 'none', width: '0%' } : undefined,
-          }}
-          transition={{
-            duration: 1,
-            ease: 'easeOut',
-            delay: curState === 0 ? 2 : 0,
-          }}
-        >
-          잭 다니엘 Old No.7
-        </RightTitle>
         {(curState === 1 || curState === 2) && (
-          <Wrapper>
-            <SecondTitle
-              animate={{
-                opacity: curState === 1 ? [0, 1] : 0,
-                y: '-20px',
-              }}
-              transition={{
-                duration: 1,
-                ease: 'easeOut',
-                delay: curState === 1 ? 2 : 0,
-              }}
-            >
-              150년의 역사
-            </SecondTitle>
-            <SecondSubTitle
-              animate={{
-                opacity: curState === 1 ? [0, 1] : 0,
-                y: '-20px',
-              }}
-              transition={{
-                duration: 1,
-                ease: 'easeOut',
-                delay: curState === 1 ? 2 : 0,
-              }}
-            >
-              시간이 증명한 완벽한 맛
-            </SecondSubTitle>
-          </Wrapper>
+          <Section>
+            <HistoryWrapper>
+              <NumberWrapper>
+                <StartNumber
+                  animate={{
+                    opacity: curState === 1 ? [0, 1] : 0,
+                  }}
+                  transition={{
+                    duration: 1,
+                    ease: 'easeOut',
+                    delay: curState === 1 ? 1.8 : 0,
+                  }}
+                >
+                  1866
+                </StartNumber>
+                <EndNumber
+                  animate={{
+                    opacity: curState === 1 ? [0, 1] : 0,
+                  }}
+                  transition={{
+                    duration: 1,
+                    ease: 'easeOut',
+                    delay: curState === 1 ? 1.8 : 0,
+                  }}
+                >
+                  2021
+                </EndNumber>
+              </NumberWrapper>
+              <LineWrapper>
+                <Bar
+                  animate={{
+                    opacity: curState === 1 ? [0, 1] : curState === 2 ? 0 : 0,
+                    width: '100%',
+                  }}
+                  transition={{
+                    duration: 1,
+                    ease: 'easeOut',
+                    delay: curState === 1 ? 1.3 : 0,
+                  }}
+                />
+                <UpBar
+                  animate={{
+                    opacity: curState === 1 ? [0, 1] : 0,
+                    width: '30px',
+                  }}
+                  transition={{
+                    duration: 0.5,
+                    ease: 'easeOut',
+                    delay: curState === 1 ? 1.5 : 0,
+                  }}
+                />
+                <DownBar
+                  animate={{
+                    opacity: curState === 1 ? [0, 1] : 0,
+                    width: '30px',
+                  }}
+                  transition={{
+                    duration: 0.5,
+                    ease: 'easeOut',
+                    delay: curState === 1 ? 1.5 : 0,
+                  }}
+                />
+              </LineWrapper>
+            </HistoryWrapper>
+            <Wrapper>
+              <SecondTitle
+                animate={{
+                  opacity: curState === 1 ? [0, 1] : 0,
+                  y: '-20px',
+                }}
+                transition={{
+                  duration: 1,
+                  ease: 'easeOut',
+                  delay: curState === 1 ? 2.5 : 0,
+                }}
+              >
+                150년의 역사
+              </SecondTitle>
+              <SecondSubTitle
+                animate={{
+                  opacity: curState === 1 ? [0, 1] : 0,
+                  y: '-20px',
+                }}
+                transition={{
+                  duration: 1,
+                  ease: 'easeOut',
+                  delay: curState === 1 ? 2.5 : 0,
+                }}
+              >
+                시간이 증명한 완벽한 맛
+              </SecondSubTitle>
+            </Wrapper>
+          </Section>
         )}
 
         <ArrowNext NextPage={NextPage} Color={'black'} Display={nextButton} />
@@ -140,10 +208,19 @@ const Background = styled(motion.div)`
   height: 100vh;
   background-color: white;
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
 
   opacity: 0;
+`;
+
+const TitleWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 `;
 
 const LeftTitle = styled(motion.div)`
@@ -157,18 +234,6 @@ const LeftTitle = styled(motion.div)`
   opacity: 0;
   top: -20px;
   font-weight: bold;
-`;
-
-const JackImg = styled(motion.div)`
-  background: url('/yun/img/jack.png');
-  position: absolute;
-  object-fit: cover;
-  width: 215px;
-  height: 600px;
-  background-repeat: no-repeat;
-  background-size: cover;
-
-  opacity: 0;
 `;
 
 const RightTitle = styled(motion.div)`
@@ -185,13 +250,101 @@ const RightTitle = styled(motion.div)`
   opacity: 0;
 `;
 
+const JackImg = styled(motion.div)`
+  background: url('/yun/img/jack.png');
+  position: absolute;
+  object-fit: cover;
+  width: 215px;
+  height: 600px;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+  opacity: 0;
+`;
 /////////////////////////////////////////
+const Section = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+const HistoryWrapper = styled(motion.div)`
+  position: absolute;
+  top: 50px;
+  left: 350px;
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const NumberWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+const StartNumber = styled(motion.div)`
+  font-size: 30px;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* opacity: 0; */
+  font-weight: bold;
+`;
+const EndNumber = styled(motion.div)`
+  font-size: 30px;
+  color: black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* opacity: 0; */
+  font-weight: bold;
+  margin-right: 50px;
+`;
+const LineWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+const Bar = styled(motion.hr)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 0%;
+  border: 3px solid black;
+`;
+const UpBar = styled(motion.hr)`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  right: 0;
+  border: 3px solid black;
+  transform: translate(16%, -180%) rotate(45deg);
+`;
+const DownBar = styled(motion.hr)`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 0px;
+  right: 0;
+  border: 3px solid black;
+
+  transform: translate(16%, 180%) rotate(-45deg);
+`;
+
 const Wrapper = styled(motion.div)`
   position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  width: 500px;
 `;
 const SecondTitle = styled(motion.div)`
   position: relative;
