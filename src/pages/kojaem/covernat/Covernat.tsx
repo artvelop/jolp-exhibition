289d92c15943875110be27e87992f164d26f2708
KoJaem <woalswhwh@naver.com>
2021-10-07 20:38:50 +0900

    커버낫 로딩 이후 어떤식으로 구현할지 틀 잡아놓음.

--- src/pages/kojaem/covernat/Covernat.tsx
+++ src/pages/kojaem/covernat/Covernat.tsx
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 import React, { useState } from 'react';
 import { LayoutContainer } from '../components/LayoutContainer';
 import { Loading } from "./components/Loading";
+import { motion } from "framer-motion";
 type Props = {
   selected: string;
   setSelected: (link:string) => void;
@@ -16,16 +17,22 @@ const Covernat = ({selected, setSelected}:Props) => {
   >
     {isLoading ?
     <CovernatWrapper>
-      로딩구현까지 완료!
+      <BrandImage
+      src={`/koJaem/img/Covernat.jpg`}
+      alt="brand"
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      />
+      <Description
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      >
+      <BrandName>Covernat</BrandName>
+      <Intro>커버낫에 대한 설명</Intro>
+      </Description>
     </CovernatWrapper> 
 
       : <Loading setIsLoading={setIsLoading} />}
-
-    
-    
-
-    
-
     
   </LayoutContainer>
   );
@@ -34,7 +41,44 @@ const Covernat = ({selected, setSelected}:Props) => {
 export default Covernat;
 
 const CovernatWrapper = styled.div`
+    display: flex;
+    align-items: center;
     width: 100vw;
     height: 100vh;
     background-color:#d7d7d5;
+`;
+
+const BrandImage = styled(motion.img)`
+  max-width: 800px;
+  overflow: hidden;
+`;
+
+const Description = styled(motion.div)`
+  display:flex;
+  flex-direction: column;
+  text-align: center;
+  
+`;
+const BrandName = styled.h1`
+  font-size: 20px;
+  color: white;
+
+  @media screen and (min-width: 600px) {
+    font-size: 30px;
+}
+
+@media screen and (min-width: 800px) {
+    font-size: 40px;
+}
+
+@media screen and (min-width: 1000px) {
+    font-size: 50px;
+}
+@media screen and (min-width: 1200px) {
+    font-size: 60px;
+}
+`;
+
+const Intro = styled.h1`
+
 `;
