f22a08b61161c7ae8259399db783a50112281fe7
KoJaem <woalswhwh@naver.com>
2021-10-13 23:03:48 +0900

    이미지배너 슬라이드 자동으로 하나씩 넘어가게 구현

--- src/pages/kojaem/covernat/components/ImageBanner.tsx
+++ src/pages/kojaem/covernat/components/ImageBanner.tsx
@@ -1,13 +1,47 @@
 import styled from "@emotion/styled";
 import { motion } from "framer-motion";
-import { THookReturn, useKeenSlider } from "keen-slider/react";
+import KeenSlider from "keen-slider";
+import { useKeenSlider } from "keen-slider/react";
+import { RefObject, useEffect, useRef, useState } from "react";
 
+type sliderType = [sliderRef: RefObject<HTMLDivElement>, slider: KeenSlider];
 export const ImageBanner = () => {
-  const [sliderRef]: THookReturn<HTMLDivElement> = useKeenSlider({
+  const [pause, setPause] = useState(false);
+
+  const timer: any = useRef(null);
+
+  const [sliderRef, slider]: sliderType = useKeenSlider({
     slidesPerView: 4,
     spacing: 10,
     loop: true,
+    duration: 1500,
+    dragStart: () => {
+      setPause(true);
+    },
+    dragEnd: () => {
+      setPause(false);
+    },
   });
+
+  useEffect(() => {
+    sliderRef.current!.addEventListener("mouseover", () => {
+      setPause(true);
+    });
+    sliderRef.current!.addEventListener("mouseout", () => {
+      setPause(false);
+    });
+  }, [sliderRef]);
+
+  useEffect(() => {
+    timer.current = setInterval(() => {
+      if (!pause && slider) {
+        slider.next();
+      }
+    }, 2000);
+    return () => {
+      clearInterval(timer.current);
+    };
+  }, [pause, slider]);
 
   return (
     <ImageUl
