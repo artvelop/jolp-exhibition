6b2d9760ee644feec03b71f1fe9a46008f1a84e5
ruso99 <hondongk@naver.com>
2021-10-13 17:34:10 +0900

    [FIX] 임시배포

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
 import Dongchurl from './pages/dongchurl/Dongchurl';
 import Yun from './pages/yun/Main';
 import KojaemRouter from './pages/kojaem/KojaemRouter';
@@ -9,7 +8,7 @@ function Root() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Dongchurl} />
         <Route path="/dongchurl" component={Dongchurl} />
         <Route path="/yun" component={Yun} />
         <Route path="/kojaem/" component={KojaemRouter} />
