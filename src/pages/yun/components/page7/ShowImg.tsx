1eb1ca991106b5339550511dbb878f69606e2416
UNNEW-main <1970005826c@gmail.com>
2021-10-18 19:01:35 +0900

    [ADD] page7 UI

--- src/pages/yun/components/page7/ShowImg.tsx
+++ src/pages/yun/components/page7/ShowImg.tsx
@@ -1,35 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import styled from '@emotion/styled';
 
-type Props = { name: number; imgX: number; imgY: number; imgW: number; imgH: number };
-export const ShowImg: React.FC<Props> = ({ name, imgX, imgY, imgW, imgH }) => {
+type Props = { name: number; imgX: number; imgY: number; imgW: number; imgH: number; dTime: number };
+export const ShowImg: React.FC<Props> = ({ name, imgX, imgY, imgW, imgH, dTime }) => {
+  const [visible, setVisible] = useState(0);
+
+  function timeout(delay: number) {
+    return new Promise((res) => setTimeout(res, delay));
+  }
+
+  useEffect(() => {
+    const wait2 = async () => {
+      await timeout(dTime);
+      setVisible(1);
+    };
+    wait2();
+  }, []);
+
   return (
-    <Wrapper
+    <Section
       animate={{
-        opacity: 1,
+        opacity: [0, 1],
       }}
       transition={{
-        duration: 0.5,
+        duration: 1,
         ease: 'easeOut',
-        delay: 0.5,
+        delay: dTime,
       }}
-      whileHover={{ scale: 1.3 }}
-      name={name}
-      imgX={imgX}
-      imgY={imgY}
-      imgW={imgW}
-      imgH={imgH}
-    />
+    >
+      <Wrapper
+        animate={{
+          opacity: visible == 1 ? [0, 1] : 0,
+        }}
+        transition={{
+          duration: 0.5,
+          ease: 'easeOut',
+        }}
+        whileHover={{ scale: 1.3, zIndex: 6 }}
+        name={name}
+        imgX={imgX}
+        imgY={imgY}
+        imgW={imgW}
+        imgH={imgH}
+      />
+    </Section>
   );
 };
 
+const Section = styled(motion.div)``;
 type WrapperProps = { name: number; imgX: number; imgY: number; imgW: number; imgH: number };
 const Wrapper = styled(motion.div)<WrapperProps>`
   position: absolute;
   background: url('${process.env.PUBLIC_URL}/yun/img/rock/${({ name }) => name.toString()}.jpg');
-  width: ${({ imgW }) => imgW.toString() + 'px'};
-  height: ${({ imgH }) => imgH.toString() + 'px'};
+  width: ${({ imgW }) => imgW.toString() + '%'};
+  height: ${({ imgH }) => imgH.toString() + '%'};
   top: ${({ imgY }) => imgY.toString() + '%'};
   left: ${({ imgX }) => imgX.toString() + '%'};
   background-repeat: no-repeat;
