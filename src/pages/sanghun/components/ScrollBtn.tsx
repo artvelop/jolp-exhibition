ba6070f3978ce95e551587b006aba1e35b1ecb44
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2021-10-31 22:05:44 +0900

    최상위로 올리기 버튼

--- src/pages/sanghun/components/ScrollBtn.tsx
+++ src/pages/sanghun/components/ScrollBtn.tsx
@@ -17,15 +17,36 @@ export const ScrollBtn: React.FC = () => {
       behavior: "smooth",
     });
   };
+  const pageUpUp = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  };
 
   return (
     <>
+      <UpUpBtn onClick={pageUpUp} />
       <UpBtn onClick={pageUp} />
       <DownBtn onClick={pageDown} />
     </>
   );
 };
 
+const UpUpBtn = styled(BsCaretUpSquareFill)`
+  width: 50px;
+  height: 50px;
+  color: ${color.gold_dark};
+  position: fixed;
+  right: 170px;
+  bottom: 10px;
+  cursor: pointer;
+  transition: transform 100ms ease-in;
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
 const UpBtn = styled(BsCaretUpSquareFill)`
   width: 50px;
   height: 50px;
