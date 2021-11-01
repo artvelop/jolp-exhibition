a56704624f9b67033bfcee12c44be6a8d7b20037
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2021-11-01 23:36:16 +0900

    Header에 motion 적용함 느낀점 : 해당 props안에서 css적으로도 바꿀수있다는게 신기하다

--- src/pages/sanghun/components/Header.tsx
+++ src/pages/sanghun/components/Header.tsx
@@ -1,31 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "@emotion/styled";
 import { Link } from "react-router-dom";
 import { harvard, philosophy, talkGod, live, myself } from "../dummy/books";
-import { MdMenuBook } from "react-icons/md";
+import { GiBurningBook } from "react-icons/gi";
 import { color } from "../constants";
+import { motion } from "framer-motion";
 
 export const Header: React.FC = () => {
+  const [show, setShow] = useState(false);
   return (
     <Head>
+      <HomeLogo
+        onClick={() => setShow(!show)}
+        whileTap={{ rotate: 1800, transition: { duration: 1 } }}
+        whileHover={{ scale: 1.3 }}
+      >
+        <GiBurningBook
+          style={{
+            backgroundColor: color.gold_dark,
+            color: "black",
+            borderRadius: "50%",
+          }}
+        />
+      </HomeLogo>
       <Link to="/sanghun/Menu">
-        <QQQ />
+        <Menu
+          animate={{
+            opacity: show ? 0 : 1,
+            transition: { duration: 1, ease: "linear" },
+          }}
+          whileHover={{ color: color.gold_light }}
+        >
+          MENU
+        </Menu>
       </Link>
       <ImgBox>
         <Link to="/sanghun/PhilosophyBook">
-          <Img src={philosophy.imgSrc} />
+          <Img
+            src={philosophy.imgSrc}
+            animate={{
+              x: show ? [-1000, 0] : [0, -1000],
+              opacity: show ? 1 : 0,
+              transition: { duration: 1.6, ease: "easeOut" },
+            }}
+            whileHover={{ scale: 1.3 }}
+          />
         </Link>
         <Link to="/sanghun/MyselfBook">
-          <Img src={myself.imgSrc} />
+          <Img
+            src={myself.imgSrc}
+            animate={{
+              x: show ? [-1000, 0] : [0, -1000],
+              opacity: show ? 1 : 0,
+              transition: { duration: 1.6, ease: "easeOut" },
+            }}
+            whileHover={{ scale: 1.3 }}
+          />
         </Link>
         <Link to="/sanghun/HarvardBook">
-          <Img src={harvard.imgSrc} />
+          <Img
+            src={harvard.imgSrc}
+            animate={{
+              x: show ? [-1000, 0] : [0, -1000],
+              opacity: show ? 1 : 0,
+              transition: { duration: 1.6, ease: "easeOut" },
+            }}
+            whileHover={{ scale: 1.3 }}
+          />
         </Link>
         <Link to="/sanghun/LiveBook">
-          <Img src={live.imgSrc} />
+          <Img
+            src={live.imgSrc}
+            animate={{
+              x: show ? [-1000, 0] : [0, -1000],
+              opacity: show ? 1 : 0,
+              transition: {
+                duration: 1.6,
+                ease: "easeOut",
+              },
+            }}
+            whileHover={{ scale: 1.3 }}
+          />
         </Link>
         <Link to="/sanghun/TalkGodBook">
-          <Img src={talkGod.imgSrc} />
+          <Img
+            src={talkGod.imgSrc}
+            animate={{
+              x: show ? [-1000, 0] : [0, -1000],
+              opacity: show ? 1 : 0,
+              transition: { duration: 1.6, ease: "easeOut" },
+            }}
+            whileHover={{ scale: 1.3 }}
+          />
         </Link>
       </ImgBox>
     </Head>
@@ -44,18 +110,27 @@ const Head = styled.div`
   align-items: center;
 `;
 
-const Img = styled.img`
+const Img = styled(motion.img)`
   aspect-ratio: 2/3;
   width: 70px;
   cursor: pointer;
 `;
-const ImgBox = styled.div`
+const ImgBox = styled(motion.div)`
   display: flex;
   gap: 20%;
+  user-select: none;
   margin-right: 400px;
 `;
-const QQQ = styled(MdMenuBook)`
+const HomeLogo = styled(motion.span)`
   margin-left: 30px;
   font-size: 70px;
   color: ${color.gray};
+  cursor: pointer;
+`;
+const Menu = styled(motion.span)`
+  margin-left: -500px;
+  font-size: 40px;
+  color: ${color.gray};
+  cursor: pointer;
+  user-select: none;
 `;
