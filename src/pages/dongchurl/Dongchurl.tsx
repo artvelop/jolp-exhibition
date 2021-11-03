8dddcb0b4b0adf06ed63257cbc77f9fd2d6f9c5c
ruso99 <hondongk@naver.com>
2021-11-03 16:23:42 +0900

    [ADD] 렌더트리 추가

--- src/pages/dongchurl/Dongchurl.tsx
+++ src/pages/dongchurl/Dongchurl.tsx
@@ -1,7 +1,9 @@
+import styled from '@emotion/styled';
 import React from 'react';
 import { Intro, LayoutContainer, StepLayout } from './components';
 import { Step1 } from './components/Step1';
 import { Step2 } from './components/Step2';
+import { Step3 } from './components/Step3';
 
 const Dongchurl = () => {
   return (
@@ -13,8 +15,16 @@ const Dongchurl = () => {
       <StepLayout>
         <Step2 />
       </StepLayout>
+      <StepLayout>
+        <Step3 />
+      </StepLayout>
+      <End />
     </LayoutContainer>
   );
 };
+
+const End = styled.div`
+  margin-top: 50vh;
+`;
 
 export default Dongchurl;
