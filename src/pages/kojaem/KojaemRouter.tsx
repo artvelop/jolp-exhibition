98dc2233e62e541008b29916133f35238f1463c8
KoJaem <woalswhwh@naver.com>
2021-11-29 13:54:19 +0900

    HOME 을 제외한 다른 페이지에서 새로고침을 하면 LayoutContainer 가 HOME 을 가리키는 오류 수정

--- src/pages/kojaem/KojaemRouter.tsx
+++ src/pages/kojaem/KojaemRouter.tsx
@@ -8,15 +8,16 @@ import { Route, Switch } from "react-router-dom";
 import { AnimatePresence, AnimateSharedLayout } from "framer-motion";
 
 const KojaemRouter = () => {
-  const [selected, setSelected] = useState("HOME");
+  const [selected, setSelected] = useState("");
 
   return (
     <>
       <Route
           render={({ location }) => (
         <AnimateSharedLayout>
-            <AnimatePresence exitBeforeEnter initial={false}>
+            <AnimatePresence exitBeforeEnter>
         <Switch location={location} key={location.pathname}>
+          {setSelected(location.pathname.split("/")[2])}
           <Route exact path="/kojaem/HOME"
             render={() => <Gallery selected={selected} setSelected={setSelected}/>}
             >
